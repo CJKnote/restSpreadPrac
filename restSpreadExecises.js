d6745cc74aa8fd3e8948fe2bc5d95c014f2dbcf9
Caroline Knote <cj.knote@gmail.com>
2021-12-21 00:31:08 -0800

    simplifying method

--- restSpreadExecises.js
+++ restSpreadExecises.js
@@ -67,8 +67,4 @@ const combine = (obj1, obj2) => ({...obj1, ...obj2});
 
 /** Return a new object with a modified key and value. */
 
-const update = (obj, key, val) => {
-    let newObj = {...obj}
-    newObj[key] = val;
-    return newObj;
-}
+const update = (obj, key, val) => ({...obj, [key]:val});
